2a58bc0b92354ef519aace06f50632bcaa73b9c3
Parviz04 <sodatov0705@gmail.com>
2025-07-03 09:15:21 +0300

    project with router and logout page

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,16 @@
+
+import '@/assets/'
+export default function Login() {
+	return (
+		<>
+   <section className='flex justify-between'>
+		<div className='border-[1px] border-solid border-black w-[45%] h-[60vh] bg-blue-400'>
+
+		</div>
+			<div className='border-[1px] border-solid border-black w-[45%]'>	
+		</div>
+	</section>
+
+		</>
+	)
+}
